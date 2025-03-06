91bf1e15eac89f25f0ec5b8706025d1c083d2aa2
Chethan-KM <chethan.km@taylorandfrancis.com>
2025-03-06 17:52:23 +0530

    update the CORS file

--- src/main/java/com/bookstore/config/CorsConfig.java
+++ src/main/java/com/bookstore/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://books-web-app.s3.ap-south-1.amazonaws.com");
+        config.addAllowedOrigin("*");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         source.registerCorsConfiguration("/**", config);
